f21f02c95f9d860c6ab7bd6ba1afbd76db5e3787
dnkilic <dnkilic@outlook.com>
2016-06-12 11:56:09 +0300

    Doğan Kılıç

--- app/src/main/java/com/dnkilic/gerzek/MainActivity.java
+++ app/src/main/java/com/dnkilic/gerzek/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("deneme","deneme");
+        Log.i("Doğan", "Kılıç");
     }
 }
