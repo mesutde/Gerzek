c21af938fd72189da413ea796600a55d7ba90db9
dnkilic <dnkilic@outlook.com>
2016-06-12 11:31:55 +0300

    Initial commit

---
+++ app/src/main/java/com/dnkilic/gerzek/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dnkilic.gerzek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*İçerik girdilerini assets dosyasında adınıza
+    açılan dosyaya alt alta ekleyiniz.*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
